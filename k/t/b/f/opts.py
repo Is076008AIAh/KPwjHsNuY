77c7a39719318bd33525f7d64c5c6d26d2e07c7b
Is076008AIAh <rink_powders0r@icloud.com>
2025-03-07 21:01:55 +0100

    Resolve a memory leak issue in the file

--- k/t/b/f/opts.py
+++ k/t/b/f/opts.py
@@ -12,7 +12,6 @@ def parse_opts():
     parser.add_argument('--channel', default=1, type=int, help='channel number of input data, 1 for Y channel, 3 for YUV')
     parser.add_argument('--size_x', default=112, type=int, help='patch size x of segment')
     parser.add_argument('--size_y', default=112, type=int, help='patch size y of segment')
-    parser.add_argument('--stride_x', default=80, type=int, help='patch stride x between segments')
     parser.add_argument('--stride_y', default=80, type=int, help='patch stride y between segments')
 
     parser.add_argument('--learning_rate', default=3e-4, type=float, help='learning rate')
